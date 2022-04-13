2b6f6b61cd2905e784c5e5ad1d4657d5eadc9e12
Caroline Barbosa <bo.varig@gmail.com>
2022-04-12 21:12:58 -0300

    create: the arrow function

--- slideshow-js/src/js/index.js
+++ slideshow-js/src/js/index.js
@@ -1,7 +1,19 @@
+'use strict';
 
 const images = [
-  {'id': '1', 'url':'../src/img/casa1.png'},
-  {'id': '2', 'url':'../src/img/casa2.png'},
-  {'id': '3', 'url':'../src/img/casa5.png'},
+  {'id': '1', 'url':'./src/img/casa1.png'},
+  {'id': '2', 'url':'./src/img/casa2.jpg'},
+  {'id': '3', 'url':'./src/img/casa5.jpg'},
 ]
 
+const container = document.querySelector('#container__items');
+
+const loadImages = (images, container) => {
+  images.forEach(image => {
+    container.innerHTML += `
+      <div class='item'>
+        <img src='${image.url}'></img>
+      </div> `
+  })
+}
+loadImages(images, container);
