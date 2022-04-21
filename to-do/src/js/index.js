c216e512424563fea0489b7e3a02690f3f19db8c
Caroline Barbosa <bo.varig@gmail.com>
2022-04-20 21:04:00 -0300

    create: the function to check and uncheck the itens

--- to-do/src/js/index.js
+++ to-do/src/js/index.js
@@ -5,13 +5,13 @@ let storage = [
   {'task': 'Netflix', 'status': 'checked'}
 ]
 
-const createItem = (task, status = '') => {
+const createItem = (task, status, index) => {
   const item = document.createElement('label');
   item.classList.add('to__do--item');
   item.innerHTML = `
-  <input type="checkbox" ${status}>
+  <input type="checkbox" ${status} data-index=${index}>
   <div>${task}</div>
-  <input type="button" value="X">
+  <input type="button" value="X" data-index=${index}>
   `
   document.getElementById('toDolist').appendChild(item);
 }
@@ -24,6 +24,38 @@ const eraseTask = () => {
 }
 const updateScreen = () => {
   eraseTask()
-  storage.forEach(item => createItem(item.task, item.status));
+  storage.forEach((item,index)  => createItem(item.task, item.status, index));
 }
+const addItem = (event) => {
+  const key = event.key;
+  const text = event.target.value
+  if (key === 'Enter') {
+    storage.push ({'task': text,'status': ''})
+    updateScreen();
+    event.target.value = '';
+  }
+}
+
+const removeItem = (index) => {
+  storage.splice (index,1);
+  updateScreen();
+}
+
+const updateItem = (event) => {
+  storage[index].status = storage[index].status === '' ? 'checked' : '';
+  updateScreen();
+}
+const clickItem = (event) => {
+  const element = event.target;
+  if (element.type === 'button') {
+    const index = element.dataset.indice;
+    removeItem(index);
+  } else if (element.type === 'checkbox'){
+    const index = element.dataset.indice;
+    updateItem(index);
+  }
+}
+
+document.getElementById('newItem').addEventListener('keypress',addItem);
+document.getElementById('toDolist').addEventListener('click', clickItem);
 updateScreen();
